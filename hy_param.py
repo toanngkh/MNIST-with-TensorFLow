102355e0b78a58cfab25279d993ab36a94db8d57
Toan Nguyen Khanh <10647699+toanngkh@users.noreply.github.com>
2018-12-20 22:27:35 -0600

    Add files via upload

---
+++ hy_param.py
@@ -0,0 +1,17 @@
+# Hyperparameters and all other kinds of params
+
+# Parameters
+learning_rate = 0.01
+num_steps = 100
+batch_size = 128
+display_step = 1
+
+# Network Parameters
+n_hidden_1 = 300
+n_hidden_2 = 300
+num_input = 784 # MNIST data input (shape 28*28)
+num_classes = 10 # number 0 - 9
+
+# Training Parameters
+checkpoint_every = 100
+checkpoint_dir = './runs/'
